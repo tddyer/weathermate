f5594dc57ec0ced5bc8d5753c5053768091c70d8
Tanner <tdyer@hawk.iit.edu>
2020-09-18 09:51:56 -0500

    began fetching data from weather api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:weathermate/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -27,11 +29,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   // uses openweathermap api to get weather data
   void getData() async {
-    Response response = await get("https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02");
+    http.Response response = await http.get(
+      'https://samples.openweathermap.org/data/2.5/weather?'
+      'lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decoded = jsonDecode(data);
+      
+      double temp = decoded['main']['temp'];
+      int condition = decoded['weather'][0]['id'];
+      String city = decoded['name'];
+      String weatherDesc = decoded['weather'][0]['description'];
+
+      print('$temp, $condition, $city, $weatherDesc');
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
     );
   }
